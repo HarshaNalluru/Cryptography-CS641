e1aa00dac63f6570d3e849057fc2435517c01539
Harsha Nalluru <harsha.nalluru1997@gmail.com>
2018-02-17 22:44:58 +0530

    generated outputs for the 1000 input pairs

--- get_input_output_pairs.py
+++ get_input_output_pairs.py
@@ -4,16 +4,26 @@ import pyperclip
 time.sleep(3)
 
 f = open('inputpairs.txt','r')
-f2 = open('input_output_pairs.txt','w+')
+f2 = open('input_output_pairs_1000.txt','w+')
+# j = 0
 for line in f:
-	ip1,ip2 = line.split('	')
+	# j+=1
+	# if j==5:
+	# 	break
+	ip1,ip2 = line.split(' ')
 	ip1 = ip1.strip()
+	ip2 = ip2.strip()
 
 	pyautogui.click(1170, 692)
-	for x in ip1:
-		pyautogui.press(x)
+	# for x in ip1:
+	# 	pyautogui.press(x)
+	pyperclip.copy(ip1)
+	pyautogui.keyDown('ctrl')
+	pyautogui.keyDown('v')
+	pyautogui.keyUp('v')
+	pyautogui.keyUp('ctrl')
 	pyautogui.press('enter')
-	time.sleep(0.2)
+	time.sleep(0.1)
 	pyautogui.click(589, 616)
 	pyautogui.doubleClick()
 	pyautogui.keyDown('ctrl')
@@ -25,10 +35,15 @@ for line in f:
 
 
 	pyautogui.click(1170, 692)
-	for x in ip2:
-		pyautogui.press(x)
+	# for x in ip2:
+	# 	pyautogui.press(x)
+	pyperclip.copy(ip2)
+	pyautogui.keyDown('ctrl')
+	pyautogui.keyDown('v')
+	pyautogui.keyUp('v')
+	pyautogui.keyUp('ctrl')
 	pyautogui.press('enter')
-	time.sleep(0.2)
+	time.sleep(0.1)
 	pyautogui.click(589, 616)
 	pyautogui.doubleClick()
 	pyautogui.keyDown('ctrl')
